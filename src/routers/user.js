bce0f16f9fd663c8abb61f1cad7b2f69339dbbf8
AhmedEssamDakrory <ahmed.edakrory@gmail.com>
2021-02-16 20:21:18 +0200

    fixing typo

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = new express.Router()
 const userController = require('../controllers/user')
 const auth = require('../middleware/user_auth')
+const auth2 = require('../middleware/admin_auth')
 
 router.get('/users/check', userController.checkUser)
 router.post('/users/signup', userController.userSignup)
 router.post('/users/signin', userController.userSignin)
 router.post('/users/logout', auth, userController.userLogout)
-router.get('/users', auth, userController.getUsers)
+router.get('/users', auth2, userController.getUsers)
 
 
 module.exports = router
